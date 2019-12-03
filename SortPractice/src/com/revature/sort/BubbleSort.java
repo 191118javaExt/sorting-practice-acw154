e99861eb9014ad16a5d389c39d19f751da5df11a
acw154 <acw154@gmail.com>
2019-12-03 09:13:17 -0500

    Bubble and Merge Sort Completed

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -4,5 +4,16 @@ public class BubbleSort {
 	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
+		int n = arr.length - 1; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                if (arr[j] > arr[j+1]) { 
+                    // swap arr[j+1] and arr[i] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
+            }
+        }
 	}
 }
--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -4,9 +4,41 @@ public class MergeSort {
 	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
+		if (arr.length < 2) {
+			return;
+		}
+		int mid = arr.length / 2;
+		int[] left = new int[mid];
+		int[] right = new int[arr.length - mid];
+		
+		for (int i=0; i < mid; i++) {
+			left[i] = arr[i];
+		}
+		for (int j=mid; j < arr.length; j++) {
+			right[j-mid] = arr[j];
+		}
+		
+		sort(left);
+		sort(right);
+		merge(arr, left, right);
 	}
 	
 	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
+		int i = 0;
+		int j = 0;
+		int count = 0;
+		while(i < left.length && j < right.length) {
+			if(left[i] <= right[j]) {
+				arr[count++] = left[i++];
+			} else {
+				arr[count++] = right[j++];
+			}
+		}
+		while(i < left.length) {
+			arr[count++] = left[i++];
+		}
+		while(j < right.length) {
+			arr[count++] = right[j++];
+		}
 	}
 }
